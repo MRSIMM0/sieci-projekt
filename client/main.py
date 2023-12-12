0268d0f4ef751cf9165473483c751a81e49a8e1c
MRSIMM0 <s.pasieczny02@gmail.com>
2023-12-12 19:56:21 +0100

    Remove signal handler registration and add print statement for client shutdown

--- client/main.py
+++ client/main.py
@@ -6,8 +6,8 @@ if __name__ == "__main__":
     address, port = get_server_details()
 
     client = Client(address, port)
-    signal.signal(signal.SIGINT, client.signal_handler)
     try:
         client.start_client()
     except KeyboardInterrupt:
+        print("Shutting down client...")
         client.signal_handler()
